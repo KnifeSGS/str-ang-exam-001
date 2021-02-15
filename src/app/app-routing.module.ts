9fa28c0f1319f71f5b01a98e043530c79c502da2
KnifeSGS <knife.sgs@gmail.com>
2021-02-15 22:17:05 +0100

    bs, fa, nav

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: AppComponent
+  },
+  {
+    path: 'heroes',
+    component: AppComponent
+  },
+  {
+    path: '**',
+    component: AppComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
